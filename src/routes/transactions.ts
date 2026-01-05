a0a03935851a43013597c15c426c4d395d776f1d
Renan Santos <renan-bs@hotmail.com>
2026-01-05 20:15:06 -0300

    fix: update transactions route to return structured responses

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,9 +5,25 @@ import { Knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const transactions = await Knex('transactions').select('*')
+    const transactions = await Knex('transactions').select()
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await Knex('transactions').where('id', id).first()
+
+    if (!transaction) {
+      return reply.status(404).send({ message: 'Transaction not found' })
+    }
+
+    return { transaction }
   })
 
   app.post('/', async (request, reply) => {
@@ -27,6 +43,8 @@ export async function transactionsRoutes(app: FastifyInstance) {
       amount: type === 'credit' ? amount : amount * -1,
     })
 
-    return reply.status(201).send('Transaction created successfully')
+    return reply
+      .status(201)
+      .send({ message: 'Transaction created successfully' })
   })
 }
