dd87f478c40dd4c04804a0b85e119ecaa29563a4
Renan Santos <renan-bs@hotmail.com>
2026-01-05 20:02:27 -0300

    feat: implement transactions routes with CRUD operations

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,32 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 import { Knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/transactions', async () => {
+  app.get('/', async () => {
     const transactions = await Knex('transactions').select('*')
 
     return transactions
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await Knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send('Transaction created successfully')
   })
 }
